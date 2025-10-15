a1582ec3378d4634c80acbd2480a95b49100681c
Kavita Karunakaran <kavitast42@gmail.com>
2025-10-15 19:43:20 +0530

    commit the code

---
+++ src/ex_04_Operators/LAb037_Operators_Logical.py
@@ -0,0 +1,17 @@
+is_kavita_married = True
+print(not is_kavita_married)
+print(is_kavita_married)
+
+# Logical Operator -> bool
+# > , <  >= <=
+x = 10
+y = 20
+print(x > y)
+print(x < y)
+
+print(" --- ")
+
+a = 10
+b = 10
+print(a == b)
+print(a >= b) # 10 > 10 or 10 = 10
---
+++ src/ex_04_Operators/Lab031_UnaryOperator.py
@@ -0,0 +1,8 @@
+age = +42
+age2 = -42
+print(age)
+print(age2)
+
+how_many_lambo_kavita = -1
+how_many_lambo_kavita = how_many_lambo_kavita + 1
+print(how_many_lambo_kavita)
