217617d1ecfe1c7149996dce9bbb3057f0791ab4
Kavita Karunakaran <kavitast42@gmail.com>
2025-11-15 10:16:23 +0530

    commit the code

--- src/Task/Task_Grade_Calc.py
+++ src/Task/Task_Grade_Calc.py
@@ -8,3 +8,25 @@
 # C: 70-79
 # D: 60-69
 # F: 0-59
+
+side1 = float(input("Enter the first side: "))
+side2 = float(input("Enter the second side: "))
+side3 = float(input("Enter the third side: "))
+
+
+def classify_triangle(a, b, c):
+    if a > 0 and b > 0 and c > 0:
+        if a + b > c and a + c > b and b + c > a:
+            if a == b == c:
+                return "Equilateral"
+            elif a == b or b == c or a == c:
+                return "Isosceles"
+            else:
+                return "Scalene"
+        else:
+            print("Not a Triangle")
+    else:
+        print("Not a Valid Length")
+
+result = classify_triangle(side1, side2, side3)
+print(f"The triangle is classified as: {result}")
---
+++ src/ex_17_Dict/Lab115_Dict_Dup_Keys.py
@@ -0,0 +1,4 @@
+p = {"name": "Kavita", "name": "Jose"}
+print(p)
+
+my_list = [1, 2, 2, 3, 4, 4, 5]
